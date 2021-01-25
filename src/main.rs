27d7ecb5cbf92626c900cf133fea784516a5b66b
Andrew Blakey <ablakey@gmail.com>
2021-01-25 10:01:33 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,13 @@ use emulator::Emulator;
 use std::env;
 
 fn main() {
-    // This is not how you do CLI argument handling. I just didn't want to bother with any
-    // additional libraries.
+
     let args: Vec<String> = env::args().collect();
     let cartridge_path = if args.len() > 1 { Some(&args[1]) } else { None };
     let skip_boot_rom = args.contains(&String::from("--noboot"));
 
     if skip_boot_rom {
-        println!("SKIP BOOT ROM");
+        println!("Skipping boot ROM and directly initializing emulator state.");
     }
 
     println!("{}", cartridge_path.unwrap());
